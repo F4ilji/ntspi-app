e99e7303629d6510e95663ec28413d02eff527ed
f4ilji <F4ilji@yandex.ru>
2024-12-25 17:18:09 +0500

    Changes

---
+++ database/migrations/2024_12_06_192028_change_content_data_type_in_educational_programs_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('educational_programs', function (Blueprint $table) {
+            $table->text('about_program')->change();
+            $table->text('program_features')->change();
+
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('educational_programs', function (Blueprint $table) {
+            $table->string('about_program')->change();
+            $table->string('program_features')->change();
+        });
+    }
+};
