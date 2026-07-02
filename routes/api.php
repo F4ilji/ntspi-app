b2fec840806153b0e2e1938102338fbef29e4c16
F4ilji <F4ilj@yandex.ru>
2026-07-02 17:05:55 +0500

    fix: empty api.php since deploy routes moved to web

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,1 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::middleware([
-    \Illuminate\Cookie\Middleware\EncryptCookies::class,
-    \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-    \Illuminate\Session\Middleware\StartSession::class,
-    'auth:web',
-])->group(function () {
-    require __DIR__.'/../app/Containers/Dashboard/UI/API/Routes/api.php';
-});
