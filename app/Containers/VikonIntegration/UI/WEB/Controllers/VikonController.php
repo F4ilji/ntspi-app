bec37c0e22caa935f651b0c95ea937e873970536
F4ilji <F4ilj@yandex.ru>
2026-07-05 22:31:51 +0500

    fix(vikon): use auth_domain for OAuth authorize URL instead of api_domain

--- app/Containers/VikonIntegration/UI/WEB/Controllers/VikonController.php
+++ app/Containers/VikonIntegration/UI/WEB/Controllers/VikonController.php
@@ -49,6 +49,7 @@ class VikonController extends Controller
             'modules' => config('vikon.modules'),
             'parts' => $parts,
             'vikon_api_domain' => config('vikon.api_domain'),
+            'vikon_auth_domain' => config('vikon.auth_domain'),
             'vikon_client_id' => config('vikon.client_id'),
         ]);
     }
@@ -89,6 +90,7 @@ class VikonController extends Controller
             'modules' => config('vikon.modules'),
             'parts' => $parts,
             'vikon_api_domain' => config('vikon.api_domain'),
+            'vikon_auth_domain' => config('vikon.auth_domain'),
             'vikon_client_id' => config('vikon.client_id'),
         ]);
     }
